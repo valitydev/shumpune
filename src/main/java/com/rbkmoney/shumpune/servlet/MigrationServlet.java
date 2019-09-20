80eb2a8dc56d34340f854b5210819fce42c2c1f4
n.pospolita <nikita7asics@gmail.com>
2019-09-20 11:57:02 +0300

    fixed context

--- src/main/java/com/rbkmoney/shumpune/servlet/MigrationServlet.java
+++ src/main/java/com/rbkmoney/shumpune/servlet/MigrationServlet.java
@@ -2,20 +2,20 @@ package com.rbkmoney.shumpune.servlet;
 
 import com.rbkmoney.damsel.shumpune.MigrationHelperSrv;
 import com.rbkmoney.woody.thrift.impl.http.THServiceBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
 @WebServlet("/migration")
+@RequiredArgsConstructor
 public class MigrationServlet extends GenericServlet {
 
 
     private Servlet thriftServlet;
 
-    @Autowired
-    private MigrationHelperSrv.Iface requestHandler;
+    private final MigrationHelperSrv.Iface requestHandler;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
--- src/main/java/com/rbkmoney/shumpune/servlet/ShumpuneServlet.java
+++ src/main/java/com/rbkmoney/shumpune/servlet/ShumpuneServlet.java
@@ -2,25 +2,25 @@ package com.rbkmoney.shumpune.servlet;
 
 import com.rbkmoney.damsel.shumpune.AccounterSrv;
 import com.rbkmoney.woody.thrift.impl.http.THServiceBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
 @WebServlet("/shumpune")
+@RequiredArgsConstructor
 public class ShumpuneServlet extends GenericServlet {
 
     private Servlet thriftServlet;
 
-    @Autowired
-    private AccounterSrv.Iface requestHandler;
+    private final AccounterSrv.Iface shumpuneServiceHandler;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         thriftServlet = new THServiceBuilder()
-                .build(AccounterSrv.Iface.class, requestHandler);
+                .build(AccounterSrv.Iface.class, shumpuneServiceHandler);
     }
 
     @Override
